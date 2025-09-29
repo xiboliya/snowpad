863b7dc96ca9945e2b1be39f6bd458c5d299a31d
chenzhengfeng <chenzhengfeng@163.com>
2025-09-29 22:06:52 +0800

    change version number

--- src/com/xiboliya/snowpad/util/Util.java
+++ src/com/xiboliya/snowpad/util/Util.java
@@ -57,7 +57,7 @@ import com.xiboliya.snowpad.setting.Setting;
  */
 public final class Util {
   public static final String SOFTWARE = "冰雪记事本"; // 软件名称
-  public static final String VERSION = "V5.3"; // 软件版本号
+  public static final String VERSION = "V6.0"; // 软件版本号
   public static final String NEW_FILE_NAME = "新建"; // 新建文件的默认名称
   public static final String OS_NAME = System.getProperty("os.name", "Windows"); // 当前操作系统的名称
   public static final String FILE_SEPARATOR = System.getProperty("file.separator", "/"); // 当前操作系统的文件分隔符
